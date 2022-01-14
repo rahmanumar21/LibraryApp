eb9c2ee5750ff2c9d85af0829f990478151bc656
arahman <rahman.poland.ce21>
2022-01-14 15:25:20 +0100

    libraryApp

---
+++ resources/views/book.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{config('app.name')}}</title>
+</head>
+<body>
+    <h1><b>Hello, everyone</b></h1>
+    <h2>This is Lab 1 Project</h2>
+    <p>Developed by {{$developerName}}</p>
+</body>
+</html>
